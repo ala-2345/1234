18b527acc410bb6a0510661749c791be60a1dfbc
ala-2345 <Alijahangirizae21@gmail.com>
2025-08-13 23:34:14 +0330

    create a program

---
+++ jet.py
@@ -0,0 +1,9 @@
+from numpy import random
+x = int(input("enter a four-digit number : "))
+while True:
+    z = random.randint(1000 , 9999)
+    print(z)
+    if x == z:
+        print("i gussed your number")
+        print(f"the password is {z}")
+        break
